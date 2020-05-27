15071bfa648a9b888ae329f5952682e3f7cc8e93
Lewis <lewisbennett669@gmail.com>
2020-05-27 11:04:56 +0100

    Added example CLI project

---
+++ Examples/Example.CLI/Program.cs
@@ -0,0 +1,23 @@
+﻿using BattleCrate.API;
+using System;
+using System.Threading.Tasks;
+
+namespace Example.CLI
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var apiClient = new ApiClient(Environment.GetEnvironmentVariable("API_KEY"));
+
+            var crates = await apiClient.Crates.ListAllCratesAsync().ConfigureAwait(false);
+
+            foreach (var crate in crates)
+            {
+                var players = await apiClient.Crates.GetPlayersAsync(crate.UUID).ConfigureAwait(false);
+                
+                Console.WriteLine($"{crate.Name} (created at {crate.CreatedAt:HH:mm, dd-MM-yyyy}) currently has {players.OnlineCount} players online.");
+            }
+        }
+    }
+}
