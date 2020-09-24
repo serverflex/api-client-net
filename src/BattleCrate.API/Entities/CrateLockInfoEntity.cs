9bea94d3a14bedaa4abe0cbdff41b52e12c042ed
Lewis <lewisbennett669@gmail.com>
2020-09-24 18:37:15 +0100

    Added LockReason schema

--- src/BattleCrate.API/Entities/CrateLockInfoEntity.cs
+++ src/BattleCrate.API/Entities/CrateLockInfoEntity.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using BattleCrate.API.Schema;
+using Newtonsoft.Json;
 
 namespace BattleCrate.API.Entities
 {
@@ -12,7 +13,7 @@ namespace BattleCrate.API.Entities
         public bool IsLocked { get; set; }
 
         /// <summary>
-        /// Gets or sets the reason why the Crate is locked, if any.
+        /// Gets or sets the reason why the Crate is locked, if any. See <see cref="LockReason" />.
         /// </summary>
         [JsonProperty("reason")]
         public string Reason { get; set; }
---
+++ src/BattleCrate.API/Schema/LockReason.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleCrate.API.Schema
+{
+    public static class LockReason
+    {
+        #region Constant Values
+        public const string Billing = "billing";
+        public const string BillingDeleteTerminationProtection = "billing_delete_termination_protection";
+        public const string BillingTrialCancelledTerminationProtection = "billing_trial_cancelled_termination_protection";
+        public const string Maintenance = "maintenance";
+        public const string TrialExpired = "trial_expired";
+        #endregion
+    }
+}
